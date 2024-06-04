8716bc44154e8832ab1bfb43a8b19748a93345eb
Sagar Kumar sah Kanu <sagarsah20834@gmail.com>
2024-06-04 14:23:59 +0545

    Config addded to backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,17 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDatabase=() =>
-{
- mongoose.connect(process.env.DB_URI,{
-     useNewUrlParser: true,
-    //  useCreateIndex: true,
-     useUnifiedTopology: true
-     }).then(con =>{
-         console.log(`MongoDB Database Connected With HOST: ${con.connection.host}`)
-     })
-}
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      //  useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB Database Connected With HOST: ${con.connection.host}`
+      );
+    });
+};
 
-module.exports=connectDatabase  
+module.exports = connectDatabase;
